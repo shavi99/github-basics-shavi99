a90e1a853670706c620e701ab7b3d012bd0a3ae0
DESKTOP-0FTG5TA\Shavi <shaviamaaa582@gmail.com>
2021-10-15 02:03:23 +0530

    db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2";
    
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'emp_db'
+  host: 'us-cdbr-east-04.cleardb.com',
+  user: 'b3dd2af6621bb1',
+  password: 'dcb28a47',
+  database: 'heroku_8c3cf6bcb95366a'
 });
   
 export default db;
