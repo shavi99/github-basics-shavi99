57a1609019c9a52b77ba381a4a5616b05893eefc
DESKTOP-0FTG5TA\Shavi <shaviamaaa582@gmail.com>
2021-10-15 17:27:02 +0530

    changed footer

--- frontend/src/components/Employelist.vue
+++ frontend/src/components/Employelist.vue
@@ -84,7 +84,7 @@
     <footer class="footer">
       <div class="content has-text-centered">
         <p>
-          Software Project Management - Project 2022
+          Software Project Management - Project 2021
         </p>
       </div>
     </footer>
